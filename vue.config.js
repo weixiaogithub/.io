b822e177ad6eb72a2d5147109e4f7d4b0758a7a7
weixiaogithub <124173783+weixiaogithub@users.noreply.github.com>
2023-02-03 23:37:56 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    productionSourceMap: false,
+    publicPath: '/',
+    outputDir: 'dist',
+    assetsDir: 'assets',
+    devServer: {
+        port: 8090,
+        host: '0.0.0.0',
+        https: false,
+        open: true
+    },
+    // 其他配置
+    
