20c0878706b7d999a94e3ca868fcff9d82fc16e1
weixiaogithub <124173783+weixiaogithub@users.noreply.github.com>
2023-02-04 11:33:59 +0800

    Delete load.js

--- load.js
+++
@@ -1,9 +0,0 @@
-/*(function() {
-	var loader = document.querySelector('.loader');
-	setTimeout(function() {
-		loader.className = loader.className + ' JS_on';		
-	}, 100);
-	setTimeout(function() {
-		loader.className = 'loader';		
-	}, 3000);
-})();*/
