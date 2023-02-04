aa0721e2fec36b78c42ee1bab01563781c9373ef
weixiaogithub <124173783+weixiaogithub@users.noreply.github.com>
2023-02-04 11:00:23 +0800

    Create load.js

---
+++ load.js
@@ -0,0 +1,9 @@
+/*(function() {
+	var loader = document.querySelector('.loader');
+	setTimeout(function() {
+		loader.className = loader.className + ' JS_on';		
+	}, 100);
+	setTimeout(function() {
+		loader.className = 'loader';		
+	}, 3000);
+})();*/
