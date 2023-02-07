ed99fa3cf5eebd972d5d13ec6d29bc9eec5adfb7
weixiaogithub <124173783+weixiaogithub@users.noreply.github.com>
2023-02-07 22:25:14 +0800

    Update back.js

--- back.js
+++ back.js
@@ -1,23 +1,16 @@
-// 控制按钮的显示和消失
- $(window).scroll(function(){
-            if($(window).scrollTop()>300){
-                $('#return-top').fadeIn(300);
-                }
-             else{$('#return-top').fadeOut(200);}
-                 })
-
-
-// 点击按钮，使得页面返回顶部
-$("#return-top").click(function(){
-scrollTo(0,0);
-});
-// 鼠标悬浮按钮之上，图片消失，文字显示
-$(".top_e").mouseover(function(){
-    $("#img").hide();
-    $("#font").show();
-})
-//鼠标离开，文字消失，图片显示。
-$(".top_e").mouseout(function(){
-    $("#font").hide();
-    $("#img").show();
-})
+// 当网页向下滑动 20px 出现"返回顶部" 按钮
+window.onscroll = function() {scrollFunction()};
+ 
+function scrollFunction() {console.log(121);
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+ 
+// 点击按钮，返回顶部
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
